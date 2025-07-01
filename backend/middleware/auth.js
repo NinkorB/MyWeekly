f01c1942a09833f9338e33e5b4894a586b54f761
NinkorB <ninkorb4@gmail.com>
2025-07-02 00:29:52 +0530

    middleware added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            req.token = token; // Pass the token along
+            next();
+        } catch (error) {
+            res.status(401).json({ msg: 'Not authorized, token failed' });
+        }
+    }
+    if (!token) {
+        res.status(401).json({ msg: 'Not authorized, no token' });
+    }
+};
+
+module.exports = protect;
