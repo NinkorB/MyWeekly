6660973e4381141d35e3b8ae93269d4bd2927b5e
Ninkor <118871069+NinkorB@users.noreply.github.com>
2025-07-02 15:17:02 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,7 @@ const taskSchema = new mongoose.Schema({
     time: { type: String, required: true },
     link: { type: String, default: '' },
     completed: { type: Boolean, default: false },
-    dayOfWeek: { type: Number, required: true }, // 1 for Monday, 7 for Sunday
+    dayOfWeek: { type: Number, required: true },
     templateId: { type: mongoose.Schema.Types.ObjectId }
 });
 
@@ -19,9 +19,8 @@ const weeklyTaskSchema = new mongoose.Schema({
     startTime: { type: Number, default: null }
 });
 
-// NEW schema for tracking weekly history
 const weekLogSchema = new mongoose.Schema({
-    weekOf: { type: Date, required: true }, // The start date of the week (a Monday)
+    weekOf: { type: Date, required: true },
     goalAchieved: { type: Boolean, required: true },
     tasksCompleted: { type: Number, required: true },
     totalTasks: { type: Number, required: true }
@@ -34,21 +33,28 @@ const userSchema = new mongoose.Schema({
     reward: { type: String, default: 'Enjoy 2 hours of guilt-free fun!' },
     dayStartTime: { type: Number, default: 9 },
     dayEndTime: { type: Number, default: 21 },
+    isAdmin: { type: Boolean, default: false },
     tasks: [taskSchema],
     weeklyTasks: [weeklyTaskSchema],
-    history: [weekLogSchema] // <-- ADDED
+    history: [weekLogSchema],
+    solvedDSA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'DSAQuestion' }]
 }, { timestamps: true });
 
-// User model methods remain the same...
 userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    if (this.username === 'ninkor') {
+        this.isAdmin = true;
+    }
     next();
 });
+
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
+
